18bba4313900c0c73c8abbf6ffeef74350a58ea5
Stephanie <contactostephaniefernandez@gmail.com>
2022-12-09 16:39:02 -0300

    Update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <div id="banner" class="banner">
-      <img src="./Images/1.svg" alt="Banner CookUp 1" />
+      <img src="./Images/1.png" alt="Banner CookUp 1" />
     </div>
   );
 };

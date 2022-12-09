29e57d08ded4cf647df7b4d35f8309b077ce004d
Stephanie <contactostephaniefernandez@gmail.com>
2022-12-09 17:41:25 -0300

    Update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div id="banner" class="banner">
     <h3>¡Explorá y descubrí todas las posibilidades que te ofrecemos acorde a tus necesidades y preferencias alimentarias!</h3>
       <img src="./Images/1.png" alt="Banner CookUp 1" />
-      <h5>Por favor, selecciona tu tipo de alimentación</h5>
+      <h5>Por favor, selecciona tu tipo de alimentación:</h5>
     </div>
   );
 };
